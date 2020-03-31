5ebcb0d8a6c68b54d5dfc4c5a3c390fde1899da4
LaiDuke <ducth1902009@fpt.edu.vn>
2020-03-31 18:26:04 +0700

    Duke

--- resources/views/detail_tour.blade.php
+++ resources/views/detail_tour.blade.php
@@ -1,12 +1,23 @@
 @extends('header')
+<style>
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+</style>
 @section('container')
-    <div class="container">
-        <div class="row">
-            <div class="col-9">
-                {!! $tour->description !!}
-            </div>
-            <div class="col-3">
+    <div class="colorlib-wrap">
+        <div class="container">
 
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="row">
+                        <div class="col-9">
+                            {!! $tour->description !!}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
