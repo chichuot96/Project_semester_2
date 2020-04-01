398e6248d238232c9dc4373019d9b5c145cca704
LaiDuke <ducth1902009@fpt.edu.vn>
2020-04-01 18:05:55 +0700

    LaiDuke

--- resources/views/detail_tour.blade.php
+++ resources/views/detail_tour.blade.php
@@ -9,12 +9,73 @@
 @section('container')
     <div class="colorlib-wrap">
         <div class="container">
-
+            <h1>
+                <strong>
+                    {{$tour->tour_name}}
+                </strong>
+            </h1>
             <div class="row">
                 <div class="col-md-9">
                     <div class="row">
-                        <div class="col-9">
+                        <div class="wrap-division">
                             {!! $tour->description !!}
+                        </div>
+                    </div>
+                </div>
+
+                <!-- SIDEBAR-->
+                <div class="col-md-3">
+                    <div class="sidebar-wrap">
+                        <div class="side search-wrap animate-box">
+                            <h3 class="sidebar-heading">Tour information</h3>
+                            <form method="post" class="colorlib-form">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="date">Where:</label>
+                                            <div class="form-field">
+                                                <input type="text" id="location" class="form-control" placeholder="Search Location">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="date">Check-in:</label>
+                                            <div class="form-field">
+                                                <i class="icon icon-calendar2"></i>
+                                                <input type="text" id="date" class="form-control date" placeholder="Check-in date">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="date">Check-out:</label>
+                                            <div class="form-field">
+                                                <i class="icon icon-calendar2"></i>
+                                                <input type="text" id="date" class="form-control date" placeholder="Check-out date">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="guests">Guest</label>
+                                            <div class="form-field">
+                                                <i class="icon icon-arrow-down3"></i>
+                                                <select name="people" id="people" class="form-control">
+                                                    <option value="#">1</option>
+                                                    <option value="#">2</option>
+                                                    <option value="#">3</option>
+                                                    <option value="#">4</option>
+                                                    <option value="#">5+</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <input type="submit" name="submit" id="submit" value="Find Flights" class="btn btn-primary btn-block">
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
